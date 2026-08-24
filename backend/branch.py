1ec5a12068e3a0c4b1eeca233846d1bc1ed585c1
hashunc <113078447+hashunc@users.noreply.github.com>
2026-08-24 12:59:16 -0400

    Basic Branch class definition

---
+++ backend/branch.py
@@ -0,0 +1,7 @@
+from user import *
+class Branch:
+    users: Dict[int, User]
+    def __init__(self, id, name, users = {}) -> None:
+        self.id = id
+        self.name = name
+        self.users = users
