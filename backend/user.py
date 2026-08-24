79ea80741d30cb902b32d4f64c4e430425d7f2be
hashunc <113078447+hashunc@users.noreply.github.com>
2026-08-24 12:50:13 -0400

    Added user superclass with customer and admin subclasses, logic for customers

---
+++ backend/user.py
@@ -0,0 +1,74 @@
+from account import *
+from typing import Dict, List
+class User:
+    username: str
+    password: str
+    def login(self, username, password) -> int:
+        if self.username == username and self.password == password:
+            return 0
+        return 1
+
+class Customer(User):
+    def __init__(self, id, name, email, branch_id, username, password) -> None:
+        self.id = id
+        self.name = name
+        self.email = email
+        self.branch_id = branch_id
+        self.accounts: Dict[int, Account] = {}
+        self.username = username
+        self.password = password
+
+    def add_account(self, account: Account) -> int:
+        if account.id in self.accounts:
+            return 1
+        self.accounts[account.id] = account
+        return 0
+
+    def view_accounts(self) -> Dict[int, Account]:
+        return self.accounts
+
+    def get_account(self, id) -> Account | None:
+        if id in self.accounts:
+            return self.accounts[id]
+        return None
+
+    def deposit(self, account_id, amount) -> int:
+        account = self.get_account(account_id)
+        # if id is not in accounts
+        if account is None:
+            return 3
+        return account.deposit(amount)
+
+    def withdraw(self, account_id, amount) -> int:
+        account = self.get_account(account_id)
+        # if id is not in accounts
+        if account is None:
+            return 3
+        return account.withdraw(amount)
+
+    def transfer(self, source_id, dest_id, amount) -> int:
+        if amount <= 0:
+            return 1
+        source = self.get_account(source_id)
+        dest = self.get_account(dest_id)
+        # if id is not in accounts
+        if source is None or dest is None:
+            return 3
+        code = source.withdraw(amount)
+        # check whether withdrawal succeeded
+        if code:
+            return code
+        code = dest.deposit(amount)
+        if code:
+            return 3 + code
+        return 0
+
+
+
+class Admin(User):
+    def __init__(self, id, name, email, username, password) -> None:
+        self.id = id
+        self.name = name
+        self.email = email        
+        self.username = username
+        self.password = password
